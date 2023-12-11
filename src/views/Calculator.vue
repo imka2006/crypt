7397e45a2c304100c60bb098294e89df3c0eef51
sexyboyfrend <31unknown12@gmail.com>
2023-12-11 22:20:20 +0600

    qwer

--- src/views/Calculator.vue
+++ src/views/Calculator.vue
@@ -3,63 +3,64 @@
     <div class="container">
       <div class="all-content">
         <form @submit.prevent="addList" class="all-block">
-          <input type="text" placeholder="Название" required v-model="name">
-          <input type="number" placeholder="процент" required v-model="percent">
-          <input type="number" placeholder="вложено денег $€₽" required v-model="money">
-          <input type="text" placeholder="время" required v-model="time">
+          <input type="text" placeholder="Название"  v-model="name">
+          <input type="text" placeholder="процент"  v-model="percent">
+          <input type="number" placeholder="вложено денег $€₽"  v-model="money">
+          <input type="text" placeholder="время"  v-model="time">
           <button class="all-add">ДОБАВИТЬ</button>
         </form>
         <Block v-for="inner in coin" :key="inner.id" :inner="inner" />
 
         <template v-if="store.state.allData.reduce((accumulator, currentValue) => {
-            return accumulator + currentValue.money;
-          }, 0) >= store.state.allData.reduce((accumulator, currentValue) => {
-            return accumulator + currentValue.nested;
-          }, 0)">
-            обящяя прибыль со всех монет: {{ store.state.allData.reduce((accumulator, currentValue) => {
+          return accumulator + currentValue.money;
+        }, 0) >= store.state.allData.reduce((accumulator, currentValue) => {
+          return accumulator + currentValue.nested;
+        }, 0)">
+          обящяя прибыль со всех монет: {{ store.state.allData.reduce((accumulator, currentValue) => {
             return accumulator + currentValue.money;
           }, 0) }}
         </template>
         <template v-else>
-            обящий убыток со всех монет: {{ store.state.allData.reduce((accumulator, currentValue) => {
+          обящий убыток со всех монет: {{ store.state.allData.reduce((accumulator, currentValue) => {
             return accumulator + currentValue.money;
           }, 0) }}
         </template>
         <br />
         <br />
         <template v-if="store.state.allData.reduce((accumulator, currentValue) => {
-            return accumulator + currentValue.cleanMoney;
-          }, 0) >= 0">
+          return accumulator + currentValue.cleanMoney;
+        }, 0) >= 0">
           обящяя чистая прибыль со всех монет: {{ store.state.allData.reduce((accumulator, currentValue) => {
             return accumulator + currentValue.cleanMoney;
           }, 0) }}
-        </template> 
+        </template>
         <template v-else>
           общий чистый убыток со всех монет: {{ store.state.allData.reduce((accumulator, currentValue) => {
             return accumulator + currentValue.cleanMoney;
           }, 0) }}
-        </template> 
+        </template>
 
-        <br/>
-        <br/>
+        <br />
+        <br />
 
         вложено: {{ store.state.allData.reduce((accumulator, currentValue) => {
-            return accumulator + currentValue.nested;
-          }, 0) }}
+          return accumulator + currentValue.nested;
+        }, 0) }}
       </div>
     </div>
   </section>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useStore } from 'vuex'
 import Block from '../components/Block.vue'
 const name = ref('')
-const money = ref()
-const time = ref('')
+const money = ref('1000')
+const time = ref('mounth')
 const percent = ref('')
 const id = ref(0)
+
 const store = useStore()
 
 const coin = ref([])
@@ -84,6 +85,7 @@ const addList = () => {
 
   console.log(coin.value);
 }
+
 
 </script>
 
